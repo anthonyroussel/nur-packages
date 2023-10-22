0bd19db3ba9b84a4aae256ee8620c3af896bd235
Anthony Roussel <anthony@roussel.dev>
2023-10-22 23:36:42 +0200

    update

--- modules/nixos/gns3-server.nix
+++ modules/nixos/gns3-server.nix
@@ -16,20 +16,12 @@ in {
 
   options = {
     services.gns3-server = {
-      enable = lib.mkOption {
-        type = lib.types.bool;
-        default = false;
-        description = lib.mdDoc ''Whether to enable GNS3 Server daemon.'';
-      };
+      enable = lib.mkEnableOption (lib.mdDoc "GNS3 Server daemon");
 
-      package = lib.mkPackageOptionMD pkgs "gns3-server" {};
+      package = lib.mkPackageOptionMD pkgs "gns3-server" { };
 
       auth = {
-        enable = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = lib.mdDoc ''Enable password based HTTP authentication to access the GNS3 server.'';
-        };
+        enable = lib.mkEnableOption (lib.mdDoc "password based HTTP authentication to access the GNS3 server");
 
         user = lib.mkOption {
           type = lib.types.nullOr lib.types.str;
@@ -44,19 +36,22 @@ in {
           description = lib.mdDoc ''
             A file containing the password of the SMTP server account.
 
+            ::: {.warning}
             This should be a string, not a nix path, since nix paths
             are copied into the world-readable nix store.
+            :::
           '';
         };
       };
 
       settings = lib.mkOption {
-        type = lib.types.submodule {
-          freeformType = settingsFormat.type;
-        };
+        type = lib.types.submodule { freeformType = settingsFormat.type; };
         default = { };
         description = lib.mdDoc ''
           The global options in `config` file in ini format.
+
+          Refer to <https://docs.gns3.com/docs/using-gns3/administration/gns3-server-configuration-file/>
+          for all available options.
         '';
       };
 
@@ -79,19 +74,11 @@ in {
           description = lib.mdDoc ''Path of the file GNS3 Server should log to.'';
         };
 
-        debug = lib.mkOption {
-          default = false;
-          type = lib.types.bool;
-          description = lib.mdDoc ''Whether to enable debug logging.'';
-        };
+        debug = lib.mkEnableOption (lib.mdDoc "debug logging");
       };
 
       tls = {
-        enable = lib.mkOption {
-          type = lib.types.bool;
-          default = false;
-          description = lib.mdDoc ''Whether to enable TLS encryption.'';
-        };
+        enable = lib.mkEnableOption (lib.mdDoc "TLS encryption");
 
         certFile = lib.mkOption {
           type = lib.types.nullOr lib.types.path;
@@ -113,36 +100,54 @@ in {
 
       dynamips = {
         enable = lib.mkEnableOption (lib.mdDoc ''Whether to enable Dynamips support.'');
-        package = lib.mkPackageOptionMD pkgs "dynamips" {};
+        package = lib.mkPackageOptionMD pkgs "dynamips" { };
       };
 
       ubridge = {
         enable = lib.mkEnableOption (lib.mdDoc ''Whether to enable uBridge support.'');
-        package = lib.mkPackageOptionMD pkgs "ubridge" {};
+        package = lib.mkPackageOptionMD pkgs "ubridge" { };
       };
 
       vpcs = {
         enable = lib.mkEnableOption (lib.mdDoc ''Whether to enable VPCS support.'');
-        package = lib.mkPackageOptionMD pkgs "vpcs" {};
+        package = lib.mkPackageOptionMD pkgs "vpcs" { };
       };
     };
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages =
-      [ cfg.package ]
+    assertions = [
+      {
+        assertion = cfg.tls.enable -> cfg.tls.certFile != null;
+        message = "Please provide a certificate to use for TLS encryption.";
+      }
+      {
+        assertion = cfg.tls.enable -> cfg.tls.keyFile != null;
+        message = "Please provide a private key to use for TLS encryption.";
+      }
+      {
+        assertion = cfg.auth.enable -> cfg.auth.user != null;
+        message = "Please provide a username to use for HTTP authentication.";
+      }
+      {
+        assertion = cfg.auth.enable -> cfg.auth.passwordFile != null;
+        message = "Please provide a password file to use for HTTP authentication.";
+      }
+    ];
+
+    environment.systemPackages = [ cfg.package ]
       ++ lib.optional cfg.dynamips.enable cfg.dynamips.package
       ++ lib.optional cfg.ubridge.enable cfg.ubridge.package
       ++ lib.optional cfg.vpcs.enable cfg.vpcs.package;
 
-    users.groups.ubridge = lib.mkIf cfg.ubridge.enable {};
+    users.groups.ubridge = lib.mkIf cfg.ubridge.enable { };
 
     security.wrappers.ubridge = lib.mkIf cfg.ubridge.enable {
       capabilities = "cap_net_raw,cap_net_admin=eip";
       group = "ubridge";
       owner = "root";
       permissions = "u=rwx,g=rx,o=r";
-      source = "${cfg.ubridge.package}/bin/ubridge";
+      source = lib.getExe cfg.ubridge.package;
     };
 
     users.users.gns3 = {
@@ -156,43 +161,47 @@ in {
         ++ lib.optional cfg.ubridge.enable "ubridge";
     };
 
-    users.groups.gns3 = {};
+    users.groups.gns3 = { };
 
     services.gns3-server.settings = lib.mkMerge [
       {
         Server = {
-          appliances_path = "/var/lib/gns3/appliances";
-          configs_path = "/var/lib/gns3/configs";
-          images_path = "/var/lib/gns3/images";
-          projects_path = "/var/lib/gns3/projects";
-          symbols_path = "/var/lib/gns3/symbols";
-
-          ubridge_path = lib.mkIf (cfg.ubridge.enable) "${cfg.ubridge.package}/bin/ubridge";
-
-          auth = if cfg.auth.enable then "True" else "False";
-          user = lib.mkIf (cfg.auth.user != null) cfg.auth.user;
-          password = lib.mkIf (cfg.auth.passwordFile != null) "@AUTH_PASSWORD@";
+          appliances_path = lib.mkDefault "/var/lib/gns3/appliances";
+          configs_path = lib.mkDefault "/var/lib/gns3/configs";
+          images_path = lib.mkDefault "/var/lib/gns3/images";
+          projects_path = lib.mkDefault "/var/lib/gns3/projects";
+          symbols_path = lib.mkDefault "/var/lib/gns3/symbols";
         };
       }
+      (lib.mkIf (cfg.ubridge.enable) {
+        Server.ubridge_path = lib.mkDefault (lib.getExe cfg.ubridge.package);
+      })
+      (lib.mkIf (cfg.auth.enable) {
+        Server = {
+          auth = lib.mkDefault (lib.boolToString cfg.auth.enable);
+          user = lib.mkDefault cfg.auth.user;
+          password = lib.mkDefault "@AUTH_PASSWORD@";
+        };
+      })
       (lib.mkIf (cfg.vpcs.enable) {
-        VPCS.vpcs_path = lib.mkIf (cfg.vpcs.enable) "${cfg.vpcs.package}/bin/vpcs";
+        VPCS.vpcs_path = lib.mkDefault (lib.getExe cfg.vpcs.package);
       })
       (lib.mkIf (cfg.dynamips.enable) {
-        Dynamips.dynamips_path = lib.mkIf (cfg.dynamips.enable) "${cfg.dynamips.package}/bin/dynamips";
+        Dynamips.dynamips_path = lib.mkDefault (lib.getExe cfg.dynamips.package);
       })
     ];
 
     systemd.services.gns3-server = let
-      commandArgs = "--config /etc/gns3/gns3_server.conf"
-        + " --host ${cfg.host}"
-        + " --pid /run/gns3/server.pid"
-        + " --port ${toString cfg.port}"
-        + lib.optionalString (cfg.log.file != null) " --log ${cfg.log.file}"
-        + lib.optionalString (cfg.log.debug) " --debug"
-        + lib.optionalString (cfg.tls.enable) " --ssl"
-        + lib.optionalString (cfg.tls.certFile != null) " --certfile ${cfg.tls.certFile}"
-        + lib.optionalString (cfg.tls.keyFile != null) " --certkey ${cfg.tls.keyFile}"
-      ;
+      commandArgs = lib.cli.toGNUCommandLineShell { } {
+        inherit (cfg) host port;
+        config = "/run/gns3/gns3_server.conf";
+        pid = "/run/gns3/server.pid";
+        log = cfg.log.file;
+        tls = cfg.tls.enable;
+        # These are implicitly not set if `null`
+        certfile = cfg.tls.certFile;
+        certkey = cfg.tls.keyFile;
+      };
     in
     {
       description = "GNS3 Server";
@@ -202,25 +211,23 @@ in {
       wants = [ "network-online.target" ];
 
       preStart = ''
-        install -m660 ${configFile} /etc/gns3/gns3_server.conf
+        install -m660 ${configFile} /run/gns3/gns3_server.conf
 
         ${lib.optionalString (cfg.auth.passwordFile != null) ''
           ${pkgs.replace-secret}/bin/replace-secret \
             '@AUTH_PASSWORD@' \
             ${cfg.auth.passwordFile} \
-            /etc/gns3/gns3_server.conf
+            ${configFile}
         ''}
       '';
 
-      path = lib.optional (flags.enableLibvirtd) pkgs.qemu;
+      path = lib.optional flags.enableLibvirtd pkgs.qemu;
 
       reloadTriggers = [ configFile ];
 
       serviceConfig = {
-        ConfigurationDirectory = "gns3";
-        ConfigurationDirectoryMode = "0750";
         ExecReload = "${pkgs.coreutils}/bin/kill -HUP $MAINPID";
-        ExecStart = "${cfg.package}/bin/gns3server ${commandArgs}";
+        ExecStart = "${lib.getExe cfg.package} ${commandArgs}";
         Group = "gns3";
         LimitNOFILE = 16384;
         LogsDirectory = "gns3";
@@ -235,7 +242,7 @@ in {
         WorkingDirectory = "/var/lib/gns3";
 
         # Hardening
-        DeviceAllow = lib.optional (flags.enableLibvirtd) "/dev/kvm";
+        DeviceAllow = lib.optional flags.enableLibvirtd "/dev/kvm";
         DevicePolicy = "closed";
         LockPersonality = true;
         MemoryDenyWriteExecute = true;
